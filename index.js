2707ba981baa72d92ab395621e5a6f9af3abfae7
Aman Jamwal <aman.jamwal95@gmail.com>
2023-08-02 15:32:53 +1200

    Add user routes and authentication

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/movies', require('./routes/movieRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
