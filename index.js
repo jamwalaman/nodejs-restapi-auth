751a292595c1ab2c8e1f8805fb8b8839aa38b952
Aman Jamwal <aman.jamwal95@gmail.com>
2023-08-02 22:51:23 +1200

    Add host

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, '0.0.0.0', () => console.log(`Server started on port ${port}`))
