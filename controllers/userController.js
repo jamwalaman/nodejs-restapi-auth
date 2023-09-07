1f54bb00863261d940ef15feb322fce568170416
Aman Jamwal <aman.jamwal95@gmail.com>
2023-09-07 21:53:53 +1200

    add express-validator for register form validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,48 +6,57 @@ const User = require('../models/User')
 
 // Register new user
 // POST /api/users
-const registerUser = asyncHandler (async (req, res) => {
-    const {name, email, password} = req.body
-    if (!name || !email || !password) {
-        res.status(400)
-        throw new Error('All fields are required')
-    }
-    // Check if user exists
-    if (await User.findOne({email})) {
-        res.status(400)
-        throw new Error('Email already in use')
-    }
-    // Hash password and create user
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-    const user = await User.create({name, email, password: hashedPassword})
-    if (user) {
-        res.status(201).json({
-            name: user.name, email: user.email, token: generateToken(user._id)
-        })
-    } else {
-        res.status(400)
-        throw new Error('Invalid user data')
-    }
-})
+const registerUser = [
+    body('email').notEmpty().escape().withMessage('Email is required'),
+    body('password').notEmpty().escape().withMessage('Password is required'),
+    body('name').notEmpty().escape().withMessage('Username is required'),
+
+    asyncHandler (async (req, res) => {
+        const {name, email, password} = req.body
+        const errors = validationResult(req)
+        const errsObject = errors.mapped()
+        if (!errors.isEmpty()) {
+            res.status(400)
+            throw new Error(JSON.stringify(errsObject))
+        }
+        // Check if user exists
+        if (await User.findOne({email})) {
+            res.status(400)
+            errsObject.regfail = 'Email already in use'
+            throw new Error(JSON.stringify(errsObject))
+        }
+        // Hash password and create user
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+        const user = await User.create({name, email, password: hashedPassword})
+        if (user) {
+            res.status(201).json({
+                name: user.name, email: user.email, token: generateToken(user._id)
+            })
+        } else {
+            res.status(400)
+            throw new Error('Invalid user data')
+        }
+    })
+]
 
 // Authenticate a user
 // POST /api/users/login
 const loginUser = [
-    body('email').notEmpty().escape().withMessage('Email is required'),
-    body('password').notEmpty().escape().withMessage('Password is required'),
+    body('emailLogin').notEmpty().escape().withMessage('Email is required'),
+    body('passwordLogin').notEmpty().escape().withMessage('Password is required'),
 
     asyncHandler (async (req, res) => {
         
-        const {email, password} = req.body
-        const user = await User.findOne({email})
+        const {emailLogin, passwordLogin} = req.body
+        const user = await User.findOne({email: emailLogin})
         const errors = validationResult(req)
         const errsObject = errors.mapped()
         if(!errors.isEmpty()) {
             res.status(400)
             throw new Error(JSON.stringify(errsObject))
         } else {
-            if ( user && (await bcrypt.compare(password, user.password)) ) {
+            if ( user && (await bcrypt.compare(passwordLogin, user.password)) ) {
                 res.json({
                     name: user.name, email: user.email, token: generateToken(user._id)
                 })
