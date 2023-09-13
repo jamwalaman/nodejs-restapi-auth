c181e45dbe58d04a5661168b2832f36a25544157
Aman Jamwal <aman.jamwal95@gmail.com>
2023-09-13 22:08:47 +1200

    Update origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 connectDB()
 
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors({origin: 'https://moviesdata-react.netlify.app/'}))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
